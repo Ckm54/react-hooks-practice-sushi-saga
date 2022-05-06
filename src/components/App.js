c3ac59a54b7f91ea741f30fa839b9bfe99791c68
Collins <mutaic84@gmail.com>
2022-05-06 23:51:49 +0300

    perform request to update eaten sushi

--- src/components/App.js
+++ src/components/App.js
@@ -13,9 +13,33 @@ function App() {
     .then((res) => res.json())
     .then((sushis) => setSushiData(sushis))
   }, [])
+
+  function handleEatSushi(sushi) {
+    fetch(`http://localhost:3001/sushis/${sushi.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        ...sushi,
+        'img_url': ''
+      })
+    })
+    .then((res) => res.json())
+    .then((updatedSushi) => {
+      const updatedList = sushiData.map((sushi) => {
+        if(sushi.id === updatedSushi.id) {
+          return updatedSushi
+        } else {
+          return sushi
+        }
+      })
+      setSushiData(updatedList)
+    })
+  }
   return (
     <div className="app">
-      <SushiContainer allSushi={sushiData}/>
+      <SushiContainer allSushi={sushiData} eatSushi={handleEatSushi}/>
       <Table />
     </div>
   );
