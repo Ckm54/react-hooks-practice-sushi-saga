96690f24194624ea78468c4432f43ecd74301f5a
Collins <mutaic84@gmail.com>
2022-05-06 23:31:24 +0300

    display 4 sushi at a go

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ allSushi }) {
+
+  const [startIndex, setStartIndex] = useState(1)
+
+  let displayingSushis = []
+  allSushi.map((sushi) => {
+    for(let i=startIndex; i < startIndex + 4; i++){
+      if(sushi.id === i){
+        displayingSushis.push(sushi)
+      }
+    }
+  })
+
+  function updateStartIndex() {
+    setStartIndex(startIndex+4)
+    // console.log(startIndex)
+    // console.log(displayingSushis)
+  }
+
+  const sushiContainer = displayingSushis.map((sushi) => <Sushi key={sushi.id} sushiItem={sushi}/>)
+  
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      { sushiContainer }
+      <MoreButton handleStartIndex={updateStartIndex}/>
     </div>
   );
 }
